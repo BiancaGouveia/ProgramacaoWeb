11be409ac6eebaf1389cf7a4097516272d04f52b
Bianca Gouveia <160188183+BiancaGouveia@users.noreply.github.com>
2026-06-03 15:45:56 +0000

    feat: criar renderização dinâmica de cards com JavaScriptl

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ProgramacaoWeb
+
+# -> Objetivos da primeira aula:
+    . criar grid de forma estática
+    . deixar um grid dinâmico com lista local (mock)
+    . consumir os dados de uma api
---
+++ script.js
@@ -0,0 +1,26 @@
+const data = [
+    {
+        image: "https://picsum.photos/300/302",
+        description: "eu sou uma imagem",
+        images: [
+            
+        ],
+        owner: {
+            name: "Fulano",
+            address: "rua das araras",
+            number: "11837465309"
+        }
+    },
+    {
+        image: "https://picsum.photos/300/302",
+        description: "eu sou uma imagem"
+    },
+    {
+        image: "https://picsum.photos/300/302",
+        description: "eu sou uma imagem"
+    },
+    {
+        image: "https://picsum.photos/300/302",
+        description: "eu sou uma imagem"
+    }
+]
